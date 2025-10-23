fb4a47e3c4a1701fd12e69112f25f39e9100d024
k.wloczewski.261@studms.ug.edu.pl <Guperek123@>
2025-10-23 20:24:00 +0200

    zadania dodatkowe

---
+++ lekcja_3/zadania_dodatkowe.py
@@ -0,0 +1,40 @@
+print("Zadanie 1")
+
+'''
+Napisz program, który zapyta i pobierze od użytkownika dwie liczby, a następnie
+wypisze na konsoli wynik tego działania, podając całości i resztę z dzielenia.
+np. dla danych 48 i 10: „48 dzielone przez 10 jest równe 4 i 8 reszty.”
+Powinni użyć fstringa, dzielenia całkowitoliczbowego i dzielenia modulo.
+'''
+
+print("Zadanie 2")
+
+'''
+Procenty. Pobierz od użytkownika liczbę, a następnie wartość procentową. Program
+ma za zadanie wyświetlić ile to jest ( % z liczby )
+'''
+
+print("Zadanie 3")
+
+'''
+Objętość sześcianu + pole powierzchni ścian sześcianu.
+'''
+
+print("Zadanie 4")
+
+'''
+Pobierz od użytkownika odległość (droga) i czas. Wylicz średnią prędkość
+'''
+
+print("Zadanie 5")
+
+'''
+Program do przeliczania m/s na km/h
+'''
+
+print("Zadanie 6")
+
+'''
+Kompy na osobę. Pobieramy ilość osób i komputerów. Liczymy ile średnio przypada
+komputerów na ucznia
+'''
